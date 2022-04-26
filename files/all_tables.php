02916f0fef4b59eabb5d1101eb82ed62dd3d0a96
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-04-27 00:02:56 +0530

    Update all_tables.php

--- files/all_tables.php
+++ files/all_tables.php
@@ -46,6 +46,9 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="docs.php">Get Started</a>
+        </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Buttons
