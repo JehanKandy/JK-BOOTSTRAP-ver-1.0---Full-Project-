70ed8217173ff5ad5675531dc96b755c95bf5aef
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-04-28 19:51:53 +0530

    Update all_buttons.php

--- files/all_buttons.php
+++ files/all_buttons.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../main_style.css">
-    <link rel="stylesheet" href="../all_style.css">
+    <link rel="stylesheet" href="../style_temp.css">
 
     <style>        
     .container {
@@ -62,11 +62,11 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Headers
+        Texts
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="all_headers.php">All Headers Styles</a>
-          <a class="dropdown-item" href="#">Headers Colors</a>
+          <a class="dropdown-item" href="all_text.php">All Text Styles</a>
+          <a class="dropdown-item" href="#">Text Colors</a>
 
         </div>
       </li>
