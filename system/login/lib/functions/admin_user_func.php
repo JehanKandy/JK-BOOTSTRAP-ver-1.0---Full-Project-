bcb2acc316ec000fd69ebdc94674c7c9089d6478
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-05-07 22:23:56 +0530

    Update admin_user_func.php

--- system/login/lib/functions/admin_user_func.php
+++ system/login/lib/functions/admin_user_func.php
@@ -1,13 +1,13 @@
 <?php 
 
 use FTP\Connection;
+session_start();
 
 include_once("config.php");
 
 
 function userReg($usern,$email,$pass1,$cpass){
-    $con = Connection();
-    
+    $con = Connection();    
 
     $check_sql = "SELECT * FROM user_tbl WHERE email = '$email' && username = '$usern'";
     $check_result = mysqli_query($con, $check_sql);
@@ -30,5 +30,43 @@ function userReg($usern,$email,$pass1,$cpass){
 }
 
 
+function login($username, $pwd){
+    $con = Connection();
+    $new_pwd = md5($pwd);
+
+    $login_sql = "SELECT * FROM user_tbl WHERE username = '$username' && pass1 = '$new_pwd'";
+    $login_result = mysqli_query($con, $login_sql);
+    $login_nor = mysqli_num_rows($login_result);
+    
+
+    if($login_nor > 0){
+        
+        $login_row = mysqli_fetch_assoc($login_result);
+        if($login_row['user_status'] == 1){
+            if($login_row['user_roll'] == 'admin'){
+
+                setcookie('login',$login_row['login_id'],time()+60*60,'/');
+                $_SESSION['loginSession'] = $login_row['login_id'];
+                header('location:lib/views/admin.php');
+
+            }
+            elseif($login_row['user_roll'] == 'user'){
+
+                setcookie('login',$login_row['login_id'],time()+60*60,'/');
+                $_SESSION['loginSession'] = $login_row['login_id'];
+                header('location:lib/views/user.php');
+
+                }
+    
+            }else{
+                return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>Account is Deactivated..!</div>&nbsp</center>";
+            }
+        }       
+
+    else{
+        return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>No Records Found..!</div>&nbsp</center>";
+    }
+
+}
 
 ?>
